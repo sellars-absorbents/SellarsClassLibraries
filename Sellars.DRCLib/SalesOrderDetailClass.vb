537be9d3d3701b7a90c765d14cf080a3cb05d2d4
ansodou <49168533+ansodou@users.noreply.github.com>
2024-09-03 16:43:47 -0500

    update total pallets from using TL to LTL

--- Sellars.DRCLib/SalesOrderDetailClass.vb
+++ Sellars.DRCLib/SalesOrderDetailClass.vb
@@ -1624,7 +1624,7 @@ Public Class SalesOrderDetailClass
             connection.Open()
             ' Define a variable for the database name so the application automatically sets up the SQL string below with the correct table names for test versus production
             Dim strSQL As String = "Select CUSTID_28 as Customer, CUSTYP_23 as CustomerType, LINNUM_28 as LINNUM, DELNUM_28 as DELNUM, PRTNUM_28 as PRTNUM, ORGQTY_28 as ORGQTY, CURQTY_28 as CURQTY, DUEQTY_28 as DUEQTY, SHPQTY_28 as SHPQTY, STATUS_28 as STATUS, SHPDTE_28 as SHPDTE, CURDUE_28 as CURDUE, CUSDUE_28 as CUSDUE, PRICE_28 as UnitPrice, DISC_28 as Discount, GLXREF_28 as GlCode, STK_28 as STK, isnull(STK_29, '') as DefaultSTK, isnull(SLSCNV_29, 1) PartSalesConversion, QuoteIssue, ShipFromWarehouse, AcknowledgedOn, isnull(CaseLength, 0) CaseLength, isnull(CaseHeight, 0) CaseHeight, isnull(CaseWidth, 0) CaseWidth, isnull(GrossWeight, 0) GrossWeight, IsNull(sodx.RunDate, CUSDUE_28) as RunDate, 
-                        case when CasesPerPalletTL is null or CasesPerPalletTL = 0 then 0 else (CURQTY_28 * IsNull(Part_Sales.SLSCNV_29, 1)) / CasesPerPalletTL end as EstimatedPalletCount,
+                        case when CasesPerPalletLTL is null or CasesPerPalletLTL = 0 then 0 else (CURQTY_28 * IsNull(Part_Sales.SLSCNV_29, 1)) / CasesPerPalletLTL end as EstimatedPalletCount,
                         (CURQTY_28 * IsNull(Part_Sales.SLSCNV_29, 1)) * IsNull(GrossWeight, 0) as TotalGrossWeight
                        From SO_Detail sod Join CUSTOMER_MASTER cm on CUSTID_28 = CUSTID_23 
                        Join ShopfloorControl..SalesOrderDetailExt sodx on sodx.ORDNUM = sod.ORDNUM_28 And sodx.DELNUM = DELNUM_28 And sodx.LINNUM = LINNUM_28 
